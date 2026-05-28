c9f12dc9c0cf2e95ad3e32cd9f4746c49f4b5463
LiJie <lijcoder@163.com>
2026-05-28 19:17:17 +0800

    refactor(harness): clean test

--- packages/harness/test/model-manager.test.ts
+++
@@ -1,5 +0,0 @@
-import { ModelManager } from "../src/model-manager.js";
-
-const modelJsonPath: string = "/Users/lijie/.jie/test/models.json";
-const modelManager = ModelManager.create(modelJsonPath);
-console.log(`modelManager.getAll.length: ${modelManager.getAll().length}`);
--- packages/harness/test/skills.test.ts
+++
@@ -1,6 +0,0 @@
-import { loadSkillsFromDir } from "../src/skills.js";
-
-const dir = "/Users/lijie/.jie/test/skills";
-
-const { skills } = loadSkillsFromDir({ dir: dir, source: "test" });
-console.log(skills);
